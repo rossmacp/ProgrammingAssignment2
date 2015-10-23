fc7d486cea0921f2b863198036345b93130873ff
Petra Ross-Macdonald <rossmacp@bms.com>
2015-10-23 14:12:48 -0400

    I have added some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## They don't do anything 
+## except let me practice forking cloning committing and pushing
 
-## Write a short comment describing this function
+
+## This is a function I am not going to attempt
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+
+## This is a function I am not going to attempt
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
